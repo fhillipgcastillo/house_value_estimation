50f10105e51285376c8f48d11f488686631a81f0
Fhillip Castillo <gerson.fhillip@gmail.com>
2020-05-11 23:05:13 -0400

    improve train model script to reuse some steps

--- chapter7/train_model.py
+++ chapter7/train_model.py
@@ -4,49 +4,73 @@ from sklearn import ensemble
 from sklearn.metrics import mean_absolute_error
 import joblib
 
-# Load the data set
-df = pd.read_csv("ml_house_data_set.csv")
+def clean_features(df, columns=[]):
+  for col in columns:
+    del df[col]
 
-# Remove the fields from the data set that we don't want to include in our model
-del df['house_number']
-del df['unit_number']
-del df['street_name']
-del df['zip_code']
+def get_xy(features, origin_df, res_column):
+  # returns X, Y numpy arrays
+  return features.to_numpy(), origin_df[res_column].to_numpy()
 
-# Replace categorical data with one-hot encoded data
-features_df = pd.get_dummies(df, columns=['garage_type', 'city'])
-
-# Remove the sale price from the feature data
-del features_df['sale_price']
-
-# Create the X and y arrays
-X = features_df.to_numpy()
-y = df['sale_price'].to_numpy()
+def show_mean_absolute_erros(model, _y, _x):
+  mse = mean_absolute_error(_y, model.predict(_x))
+  print("Training Set Mean Absolute Error: %.4f" % mse)
+  
+def get_train_test_splitted(features_df, df, res_column):
+  # Create the X and y arrays
+  X, y = get_xy(features_df, df, res_column)
 # Split the data set in a training set (70%) and a test set (30%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+  return train_test_split(X, y, test_size=0.3, random_state=0)
 
-# Fit regression model
-model = ensemble.GradientBoostingRegressor(
-    n_estimators=1000,
+def train_model(model, X_train, y_train):
+  print("Training...")
+  model.fit(X_train[0:100], y_train[0:100])
+  print ("Model trained")
+  return model
+
+def create_model():
+  return ensemble.GradientBoostingRegressor(
+    n_estimators=3000,
     learning_rate=0.1,
     max_depth=6,
     min_samples_leaf=9,
     max_features=0.1,
     loss='huber',
     random_state=0
-)
+  )
 
-print("Training...")
-model.fit(X_train, y_train)
+def get_trained_model( X_train, y_train ):
+  return train_model(create_model(), X_train, y_train)
+
+# Step 1: get data
+# Load the data set
+df = pd.read_csv("ml_house_data_set.csv")
+
+# Step 2: clean data for ml
+# Remove the fields from the data set that we don't want to include in our model
+clean_features(df, ['house_number','unit_number', 'street_name','zip_code'])
+
+# Step 3: use on-hot encoded data 
+# Replace categorical data with one-hot encoded data
+features_df = pd.get_dummies(df, columns=['garage_type', 'city'])
+
+# Step 4: remove result column from features
+# Remove the sale price from the feature data
+# Reuse existent procedure/function
+res_column = 'sale_price'
+clean_features(features_df, [res_column])
+
+# Step 4: get x,y train and test data
+X_train, X_test, y_train, y_test =  get_train_test_splitted(features_df, df, res_column)
+
+# Fit regression model with train data
+model = get_trained_model(X_train, y_train)
 
 # Save the trained model to a file so we can use it in other programs
-joblib.dump(model, 'trained_house_classifier_model.pkl')
+# joblib.dump(model, 'trained_house_classifier_model.pkl')
 
 # Find the error rate on the training set
-mse = mean_absolute_error(y_train, model.predict(X_train))
-print("Training Set Mean Absolute Error: %.4f" % mse)
-
+show_mean_absolute_erros(model, y_train, X_train)
 # Find the error rate on the test set
-mse = mean_absolute_error(y_test, model.predict(X_test))
-print("Test Set Mean Absolute Error: %.4f" % mse)
+show_mean_absolute_erros(model, y_test, X_test)
 
